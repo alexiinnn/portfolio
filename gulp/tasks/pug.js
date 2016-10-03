ea82591aca125f63ad3ece74cf992729460fe0f9
alexey <theblowupstudio@gmail.com>
2016-10-03 12:07:52 +0300

    5 commit troubles with 'my works' header ready all pages are incompleted

--- gulp/tasks/pug.js
+++ gulp/tasks/pug.js
@@ -4,12 +4,12 @@ module.exports = function() {
   $.gulp.task('pug', function() {
     return $.gulp.src('./source/template/pages/*.pug')
       .pipe($.gp.pug({ pretty: true }))
-      .on('error', $.gp.notify.onError(function(error) {
-        return {
-          title: 'Pug',
-          message:  error.message
-        }
-       }))
+      //.on('error', $.gp.notify.onError(function(error) {
+      //  return {
+      //    title: 'Pug',
+      //    message:  error.message
+      //  }
+      // }))
       .pipe($.gulp.dest($.config.root));
   });
 };
