4aa9895ea56d3bf427680890b3a44083a74a2cff
alexey <theblowupstudio@gmail.com>
2016-10-14 20:13:55 +0300

    8 commit troubles with triangles color some JS HTML and CSS almost done all pages flip done angles done sliders aren't complete

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,50 @@
-// NAVigation
-$('#toggle').click(function() {
-    $(this).toggleClass('active');
-    $('#overlay').toggleClass('open');
-});
+var eventListeners = (function () {
+    var init = function () {
+        _setUpListeners();
+    };
+    
+    var _setUpListeners = function () {
+        $('#toggle').on('click', _toggleNav);
+        $('.angle-down').on('click', _angleDownScroll);
+        $('.angle-up').on('click', _angleUpScroll);
+        $('.auth__toggle-btn').on('click',_logIn);
+        $('.btn-home').on('click',_home);
+    };
+
+    // NAVigation
+    var _toggleNav = function(ev) {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    };
+
+    //angle-down
+    var _angleDownScroll = function (ev){
+        var height = $(window).height();
+        $('html,body').animate({scrollTop: height}, 500);
+    };
+
+    //angle-up
+    var _angleUpScroll = function (ev){
+        $('html,body').animate({scrollTop: 0}, 500);
+    };
+
+    //log in toggle
+    var _logIn = function (ev) {
+        $('.auth__toggle-btn').css('visibility','hidden');
+        $('.welcome__menu').css('transform','rotateY(180deg)');
+    };
+
+    //home toggle
+    var _home = function (ev) {
+        $('.auth__toggle-btn').css('visibility','visible');
+        $('.welcome__menu').css('transform','rotateY(0deg)');
+    };
+
+
+    return {
+            init: init
+        };
+
+})();
+
+eventListeners.init();
