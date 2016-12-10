74a7b119db1436d793257373829d721109a90d85
alexey <theblowupstudio@gmail.com>
2016-12-10 16:32:33 +0300

    4 commit it should be fromtend commit circleSkilles animated JS+SVG
    nodeJS+express+mongoose
    nodemon fix: watch, ext, ignore options
    added all needed files
    
    issues:
    sliders no transition
    nav_overlay
    nodemon+browsersync

---
+++ source/js/circleSkill.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Alexey on 09-Dec-16.
+ */
+//SVG animated circle levels
+(function () {
+    var isRendered = 0;
+    var skillCirclesObj = [];
+    $.each($('.skill-circle'), function (ind, val) {
+        var circleBottom = $(this).height() + $(this).offset().top;
+        skillCirclesObj.push({
+            circle: val,
+            circleBottom: circleBottom,
+            isRendered: false
+        })
+        isRendered++;
+        console.log('1 '+ isRendered);
+    })
+
+    var skillCircleSet = function (ev) {
+        var scrollBottom = $(window).scrollTop() + $(window).height();
+        console.log(scrollBottom);
+        console.log('4 '+isRendered);
+        $.each(skillCirclesObj, function (ind, val) {
+            if (!val['isRendered'] && scrollBottom > val['circleBottom']) {
+                var $circle = $(val['circle']);
+                var $circleLevel = $circle.attr('data-level');
+                var $circleBar = $circle.children('.circle-bar');
+                var $calcCircleLevel = ($circleBar.css('stroke-dashoffset').slice(0, -2)) * (100 - $circleLevel) / 100;
+                $circleBar.css('stroke-dashoffset', $calcCircleLevel);
+                val['isRendered'] = true;
+                isRendered--;
+            }
+        });
+        if (isRendered<=0){
+            //shutdown eventListener
+            $(window).off('scroll', skillCircleSet);
+        }
+    };
+
+    if (isRendered > 0) {
+        $(window).on('scroll', skillCircleSet);
+    };
+
+})();
