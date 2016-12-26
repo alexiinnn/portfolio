15afec2d35a9628af8660010ccaf0b4e73bd83aa
alexey <theblowupstudio@gmail.com>
2016-12-26 05:02:58 +0300

    mouse parallax bg position fix

--- source/js/parallaxMouse.js
+++ source/js/parallaxMouse.js
@@ -2,17 +2,38 @@
 //Mouse parallax
 
 var layer = $('.parallax').find('.parallax__layer');
+
+//set backround size
+var setBgSize = function () {
+    var lastBg = layer.first().find('img');
+    var bgImgWidthRate = (document.documentElement.scrollHeight * lastBg.prop('naturalWidth') / (window.innerWidth * lastBg.prop('naturalHeight')));
+    if (bgImgWidthRate < 1) {
+        var bgImgHeightRate = 1 / bgImgWidthRate;
+        var bgImgHeightShift = (1 - bgImgHeightRate* 1.1) / 2; //1.1 = 110% // 5% for every side of screen
+        layer.find('img').css({'bottom': 100 * bgImgHeightShift + '%'});
+    }
+    else
+        layer.find('img').css({'width': 100 * bgImgWidthRate + '%'});
+
+}
+
+setBgSize();
+
+$(window).on('resize', function (ev) {
+    setBgSize(ev);
+});
+
 $(window).on('mousemove', function (ev) {
     var mouseX = ev.pageX,
         mouseY = ev.pageY,
-        wFromCenter = window.innerWidth/2 - mouseX,
-        hFromCenter = window.innerHeight/2 - mouseY;
+        wFromCenter = window.innerWidth / 2 - mouseX,
+        hFromCenter = window.innerHeight / 2 - mouseY;
 
     layer.map(function (key, value) {
-        var widthShift = wFromCenter * ((key + 1)/100);
-        var heightShift = hFromCenter * ((key + 1)/100);
+        var widthShift = wFromCenter * ((key + 1) / 100);
+        var heightShift = hFromCenter * ((key + 1) / 100);
         $(value).css({
-            'transform':'translate3d(' + widthShift + 'px, ' + heightShift + 'px, 0px)'
+            'transform': 'translate3d(' + widthShift + 'px, ' + heightShift + 'px, 0px)'
         });
     })
 });
