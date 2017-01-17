908a4ab21999c0d14784705187afc15fba111e01
alexey <theblowupstudio@gmail.com>
2017-01-17 16:34:07 +0300

    fix header height add 4th work parallaxScroll for desktop only

--- source/js/parallaxScroll.js
+++ source/js/parallaxScroll.js
@@ -1,22 +1,22 @@
 //
 // PARALLAX SCROLL
-var parallax = (function(){
+var parallax = (function () {
     var bg = $('.header__bg'),
         user = $('.avatar'),
         sectionText = $('.header__bg-text-img');
 
     return {
-        move : function (block, windowScroll, strafeAmount) {
+        move: function (block, windowScroll, strafeAmount) {
             var strafe = windowScroll / -strafeAmount + '%',
                 transformString = 'translate3d(0,' + strafe + ', 0)';
 
             block.css({
-                'transform' : transformString,
-                '-webkit-transform' : transformString
+                'transform': transformString,
+                '-webkit-transform': transformString
             });
         },
 
-        init : function (wScroll) {
+        init: function (wScroll) {
             this.move(bg, wScroll, 45);
             this.move(sectionText, wScroll, 8);
             this.move(user, wScroll, 5);
@@ -24,7 +24,9 @@ var parallax = (function(){
     }
 }());
 
-$(window).scroll(function(){
-    var wScroll = $(window).scrollTop();
-    parallax.init(wScroll);
-}); // -> scroll_end;
+if (window.innerWidth>992) {
+    $(window).scroll(function () {
+        var wScroll = $(window).scrollTop();
+        parallax.init(wScroll);
+    }); // -> scroll_end;
+}
